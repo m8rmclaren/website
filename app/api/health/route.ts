8214360fbb9d03d40c136a9bd841b5d633759dec
Hayden Roszell <hroszell@gmail.com>
2025-06-16 15:43:13 -0600

    chore(website): Add health check route
    Signed-off-by: Hayden Roszell <hroszell@gmail.com>

---
+++ app/api/health/route.ts
@@ -0,0 +1,10 @@
+export async function GET() {
+    return new Response(
+        JSON.stringify({ status: 'OK', timestamp: new Date().toISOString() }),
+        {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        }
+    );
+}
+
